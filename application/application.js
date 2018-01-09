7206e58cdd6f5d8dcfd33ca83fea0a53c2801487
dshster <dmitry@shvalyov.ru>
2018-01-10 00:20:15 +0300

    [FD-32] Add Redux module and empty storage.

--- application/application.js
+++ application/application.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from './store';
 
 const Application = () => (
-  <div>
-    <h1>Link Keeper</h1>
-  </div>
+  <Provider store={store}>
+    <div>
+      <h1>Link Keeper</h1>
+    </div>
+  </Provider>
 );
 
 export default Application;
