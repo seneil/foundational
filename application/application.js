83770d568b7812a22587d61beafb2eade88f98fb
dshster <dmitry@shvalyov.ru>
2018-01-10 23:33:32 +0300

    [FD-32] Adding styled-components module.

--- application/application.js
+++ application/application.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
+import styled from 'styled-components';
+
 import store from './store';
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 const Application = () => (
   <Provider store={store}>
-    <div>
-      <h1>Link Keeper</h1>
-    </div>
+    <Wrapper>
+      <Title>Link Keeper</Title>
+    </Wrapper>
   </Provider>
 );
 
